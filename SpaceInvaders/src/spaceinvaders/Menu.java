bebca1a54062346e5a6330bcee0814f96cb02f38
Gustavo-Lackeski <glackeski@gmail.com>
2016-05-17 16:41:30 -0300

    conectando....

--- SpaceInvaders/src/spaceinvaders/Menu.java
+++ SpaceInvaders/src/spaceinvaders/Menu.java
@@ -84,6 +84,7 @@ public class Menu extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        new SpaceInvaders(2);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
--- SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
+++ SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
@@ -1,7 +1,11 @@
 package spaceinvaders;
-
 import Util.Commons;
+import static Util.Commons.BOMB_HEIGHT;
+import static Util.Commons.BOMB_WIDTH;
+import java.awt.Image;
+import static java.lang.System.*;
 import javax.swing.*;
+import javax.swing.ImageIcon;
 
 public class SpaceInvaders extends JFrame implements Commons {
 
@@ -19,6 +23,23 @@ public class SpaceInvaders extends JFrame implements Commons {
     public static void main(String[] args) {
         Menu calc = new Menu();
         calc.setVisible(true);
+        new LoadingIcon();
+     //   Loading load = new Loading();
+        
    //     new SpaceInvaders();
     }
+}
+class LoadingIcon{    
+        private final String gif = "/Recursos/loading.gif";
+        public LoadingIcon(){
+            JFrame load = new JFrame("Loading");
+            ImageIcon loading = new ImageIcon(this.getClass().getResource(gif));  
+          //  Image image = loading.getImage();
+        //    Image newimg = image.getScaledInstance(500, 500,  java.awt.Image.SCALE_SMOOTH);
+        //    loading = new ImageIcon(newimg);
+            load.add(new JLabel("Conectando... ", loading, JLabel.CENTER));
+            load.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            load.setSize(400, 300);
+            load.setVisible(true);
+        }
 }
